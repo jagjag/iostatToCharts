7494e407d67239f28467638282f9e34cc5b4cd1d
jagjag <kyukou@gmail.com>
2015-12-16 13:03:18 +0800

    Initial commit

---
+++ oraclevg1-oraclelv1/rs.py
@@ -0,0 +1,33 @@
+
+import matplotlib  
+matplotlib.use('Agg')  
+  
+import matplotlib.pyplot as plt # import plot,savefig  
+import time
+import datetime
+def main():
+
+    realYdot= [ y.strip() for y in open('rs.log') ]
+  #y= [ i for i in range(1,len(realYdot)+1) ]
+   
+    strTime="2015-12-10 20:00:00"
+    timeArray = time.strptime(strTime, "%Y-%m-%d %H:%M:%S")
+    timeStamp = float(time.mktime(timeArray)) 
+    dateArray = datetime.datetime.utcfromtimestamp(timeStamp)
+   
+    realXdot=list()
+    adjDt=dateArray
+    #for i in range(1,len(realYdot+1):
+    i=len(realYdot)
+    while(i): 
+        adjDt= adjDt + datetime.timedelta(seconds = 10)
+        print(adjDt) 
+        realXdot.append(adjDt)
+        i=i-1
+    plt.figure(figsize=(150,15))
+    plt.plot(realXdot,realYdot,linewidth=0.8)
+    plt.savefig('rs.png')     
+
+if __name__ == '__main__':
+    main()
+
