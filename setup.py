ba64da61124431bced059b9a45868c2b7cf3b9a4
jagjag <kyukou@gmail.com>
2015-12-28 10:43:31 +0800

    clean up

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name='iostatToCharts',
+        version='',
+        packages=[''],
+        url='',
+        license='',
+        author='JJmomark',
+        author_email='kyukou@163.com',
+        description=''
+)
